be82adfb83fd7c585e5f0f903dcadc23d9c8023c
andreas <andreas.kuendig@gmx.ch>
2016-01-02 02:50:19 +0100

    listeners

--- js/looper.js
+++ js/looper.js
@@ -1,19 +1,32 @@
 var looper = {};
 (function(){
 
-    var lines = [],
-        lastLoopStart;
-    
+    var lines = [];
+    var first = true;
     var draw = function(){
-        var now = Date.now(),
-            duration = now - lastLoopStart;
-        var maxDuration = 0;
         lines.forEach(function(line){
-            line.redraw(now);
+            line.redraw(Date.now());
         });
         paper.view.draw();
+        if(first){
+            console.log(paper);
+            first = false;
+        }
     };
-    
+
+    var installListeners = function(){
+        var p = {};
+        paper.install(p);
+        var t = new p.Tool();
+        t.onMouseDown = function(e){ 
+            // for some reason this is not enough to create a paperjs ToolEvent
+            console.log("down",e);
+        };
+        t.onMouseDrag = function(e){ 
+            // for some reason this is not enough to create a paperjs ToolEvent
+            console.log("drag",e);
+        };
+    };
     var init = function(canvasId){
         var canvas = document.getElementById(canvasId);
         paper.setup(canvas);
@@ -21,7 +34,7 @@ var looper = {};
         var start = [0,20,10];
         for(var j = 0; j < 3; j++){
             var x = Math.random() * 100, y = Math.random() * 100;
-            lines[j] = makeLine(j % 2 != 0 ? 0 : 7000);
+            lines[j] = makeLine(j % 2 != 0 ? 0 : 3000);
             for(var i=0; i< durations[j]; i++){
                 x += (Math.random()-.25)*10;
                 y += (Math.random()-.25)*10;
@@ -29,13 +42,21 @@ var looper = {};
             };    
         }
 
+        // paper.tool.onMouseDown = function(event){
+        //     console.log(event);
+        // };
+
         var render = function(){
             draw();
             requestAnimationFrame(render);
         };
         lastLoopStart = Date.now();
         render();
+        installListeners ();
+
+
     };
 
     looper.init = init;
 })();
+
--- js/makeLine.js
+++ js/makeLine.js
@@ -1,39 +1,31 @@
-var makeLine;
-(function(){
-    var findSegmentIndex = function(times, time){
-        var i = 0;
-        for(; i < times.length; i++){
-            if(times[i] > time){
-                return i  ;
+var makeLine = function(duration){
+    var lastLoopStart,
+        loopDuration,
+        segments = [], 
+        times = [],
+        path = new paper.Path();
+    path.strokeColor = 'black';
+    return {
+        pushSegment: function(segment, time){
+            segments.push(segment);
+            times.push(time);
+            if(duration){
+                loopDuration = Math.ceil(time / duration) * duration;
+            }else{
+                loopDuration = time + 100;
+            }
+        },
+        redraw: function(now){
+            lastLoopStart = lastLoopStart || now;
+            var elapsed = now - lastLoopStart,
+                segmentsToShow = segments.filter(function(s, i){
+                    return times[i] < elapsed;
+                });
+            path.removeSegments();
+            path.addSegments(segmentsToShow);
+            if(elapsed - loopDuration > 0){
+                lastLoopStart = now;
             }
         }
-        return i;
     };
-
-    makeLine = function(duration){
-        var lastLoopStart,
-            loopDuration,
-            segments = [], 
-            times = [],
-            path = new paper.Path(),
-            line = {
-                pushSegment: function(segment, time){
-                    segments.push(segment);
-                    times.push(time);
-                    loopDuration = duration || time + 100;
-                },
-                redraw: function(now){
-                    lastLoopStart = lastLoopStart || now;
-                    var elapsed = now - lastLoopStart,
-                        segmentIndex = findSegmentIndex(times, elapsed);
-                    path.removeSegments();
-                    path.addSegments(segments.slice(0, segmentIndex));
-                    if(elapsed - loopDuration > 0){
-                        lastLoopStart = now;
-                    }
-                }
-            };
-        path.strokeColor = 'black';
-        return line;
-    };
-})();
+};
