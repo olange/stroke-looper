2859d7545e7147eac129f60130cafaf9caccc0e8
andreas kundig <andreas.kuendig@gmx.ch>
2016-04-12 20:32:50 +0200

    new test

--- tests/test.js
+++ tests/test.js
@@ -1,5 +1,13 @@
 var webdriverio = require('webdriverio');
 var assert = require('assert');
+// var selenium = require('selenium-standalone');
+// selenium.start(function(err, child) {
+//     console.log(err);
+//   // child.stderr.on('data', function(data){
+//   //   console.log(data.toString());
+//   // });
+// });
+
 var options = {
     desiredCapabilities: {
         browserName: 'firefox'
@@ -10,7 +18,6 @@ var dir = __dirname.substr(0,__dirname.indexOf('tests'));
 dir = dir.replace(/\\/g, '/');
 var urlIndex = 'file:///' + dir + 'index.html';
 var urlTest = 'file:///' + dir + 'test.html';
-
 var error;
 webdriverio.remote(options).init().url(urlIndex)
     .title(function(err,res) {
@@ -31,33 +38,41 @@ webdriverio.remote(options).init().url(urlIndex)
         assert.deepEqual([20,50], line.segments[0]);
         assert.deepEqual([30,70], line.segments[1]);
     })
-    .then(function(a){console.log('tests ok');},
-            function(e){console.error("FAILED: ",e.message);error = e;})
-    .end();
-
-webdriverio.remote(options).init().url(urlTest)
-    //.waitUntil(function(){ return typeof(looper)!== 'undefined' ;})
-    .execute(function(){
-        looper.setup('myCanvas', 2000);
-        var line = looper.newLine(looper.correctNow(Date.now()));
-        
-        setTimeout(function(){
-            looper.drawPoint(100,50,looper.correctNow(Date.now()));
-        }, 500);
-        setTimeout(function(){
-            looper.drawPoint(150,100,looper.correctNow(Date.now()));
-        }, 600);
-        setTimeout(function(){
-            looper.drawPoint(250,100,looper.correctNow(Date.now()));
-        }, 700);
-        setTimeout(function(){
-            looper.completeLine(looper.correctNow(Date.now()));
-        }, 800);
-        setTimeout(function(){
-            console.log(line.exportData());
-        }, 900);
-    })
-    .pause(10000)
-    .then(function(a){console.log('tests ok');},
+    .then(function(a){console.log('test 1 ok');},
           function(e){console.error("FAILED: ",e.message);error = e;})
     .end();
+
+
+
+webdriverio.remote(options).init().url(urlTest)
+    .execute(function(){
+        looper.setup('myCanvas', 2000);
+        looper.setLifetime(100);
+        window.start = 1460480954631;
+        var ms = start;
+        window.line = looper.newLine(looper.correctNow(new Date(ms)));
+        looper.drawPoint(100,50,looper.correctNow(new Date(ms+=50)));
+        looper.drawPoint(110,60,looper.correctNow(new Date(ms+=50)));
+        looper.drawPoint(120,80,looper.correctNow(new Date(ms+=50)));
+        looper.completeLine(looper.correctNow(new Date(ms+=50)));
+        return line.exportData().times;        
+    }).then(function(ret){ 
+        assert.deepEqual([50, 100, 150], ret.value);
+    }).execute(function(){
+        window.segmentToXy = function(s){return [s.point.x, s.point.y];};
+        return line.segmentsToShow(start + 101).map(segmentToXy);
+    }).then(function(ret){ 
+        assert.deepEqual([[100, 50], [110, 60]], ret.value);
+    }).then(function(a){console.log('test 2 ok');},
+            function(e){console.error("FAILED: ",e.message);error = e;})
+    .end() ;
+
+
+
+
+
+
+
+
+
+
