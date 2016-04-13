2fb25a12b75b77bee5fe02e014ad236182448487
andreas <andreas.kuendig@gmx.ch>
2016-04-14 00:45:04 +0200

    chai

--- tests/test.js
+++ tests/test.js
@@ -46,23 +46,21 @@ webdriverio.remote(options).init().url(urlIndex)
 
 webdriverio.remote(options).init().url(urlTest)
     .execute(function(){
+        var assert = chai.assert;
         looper.setup('myCanvas', 2000);
         looper.setLifetime(100);
-        window.start = 1460480954631;
+        var start = 1460480954631;
         var ms = start;
-        window.line = looper.newLine(looper.correctNow(new Date(ms)));
+        var line = looper.newLine(looper.correctNow(new Date(ms)));
         looper.drawPoint(100,50,looper.correctNow(new Date(ms+=50)));
         looper.drawPoint(110,60,looper.correctNow(new Date(ms+=50)));
         looper.drawPoint(120,80,looper.correctNow(new Date(ms+=50)));
         looper.completeLine(looper.correctNow(new Date(ms+=50)));
-        return line.exportData().times;        
-    }).then(function(ret){ 
-        assert.deepEqual([50, 100, 150], ret.value);
-    }).execute(function(){
-        window.segmentToXy = function(s){return [s.point.x, s.point.y];};
-        return line.segmentsToShow(start + 101).map(segmentToXy);
-    }).then(function(ret){ 
-        assert.deepEqual([[100, 50], [110, 60]], ret.value);
+        var data = line.exportData();        
+        assert.deepEqual([50, 100, 150], data.times);
+        var segmentToXy = function(s){return [s.point.x, s.point.y];};
+        var segs = line.segmentsToShow(start + 101).map(segmentToXy);
+        assert.deepEqual([[100, 50], [110, 60]], segs);
     }).then(function(a){console.log('test 2 ok');},
             function(e){console.error("FAILED: ",e.message);error = e;})
     .end() ;
